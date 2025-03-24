4528bf29069186e529fd8e07500d2574ef26fd07
BC100Dev <bechris.100@outlook.com>
2025-03-24 12:35:15 +0100

    Halt progress (progress of creating "lappkg")

--- Sources/AppGUI/SourceInfo.cmake
+++ Sources/AppGUI/SourceInfo.cmake
@@ -4,5 +4,4 @@ include_directories("${PROJECT_MODULE_ROOT}/SharedData")
 
 add_executable(RunAsGPU ${ModSources} ${PROJECT_MODULE_ROOT}/Resources/resources.qrc)
 
-message(STATUS "GUI Resources: ${GUI_RESOURCES}")
 target_link_libraries(RunAsGPU PRIVATE RunAsGPU-shared Qt6::Widgets Qt6::Gui Qt6::Core)
